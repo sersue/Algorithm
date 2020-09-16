cb4a5ea9923a4a2f40030da468c860cbf9fc9a42
MoonSueLym <msr56501354@gmail.com>
2020-09-16 17:30:22 +0900

    baekjoon

---
+++ 10818.py
@@ -0,0 +1,12 @@
+a = int(raw_input())
+
+numbers=[]
+number = map(int,raw_input().split())
+numbers.append(number)
+print (numbers)
+
+
+
+# mi = min(numbers)
+# ma = max(numbers)
+# print('%d %d' %(mi,ma))
