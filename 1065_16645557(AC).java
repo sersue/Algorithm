b721157a47a7456d7695e3a9e5c343076d6e7964
MoonSueLym <msr56501354@gmail.com>
2021-01-11 21:59:53 +0900

    codeup_1065

---
+++ 1065_16645557(AC).java
@@ -0,0 +1,24 @@
+﻿import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main{
+    public static void main (String[] args) {
+        ArrayList <Integer> sl = new <Integer> ArrayList();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        sl.add(a);
+        sl.add(b);
+        sl.add(c);
+        for(int i=0;i<3;i++){
+            if(sl.get(i)%2==0){
+                System.out.println(sl.get(i));
+            }
+        }
+       
+
+
+        
+    }
+}
