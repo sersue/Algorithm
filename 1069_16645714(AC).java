28a42e3ee20a8e406abe3caa0524de4b74b2ddcd
MoonSueLym <msr56501354@gmail.com>
2021-01-11 22:28:35 +0900

    codeup_

---
+++ 1069_16645714(AC).java
@@ -0,0 +1,26 @@
+﻿import java.util.Scanner;
+
+public class Main{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char command = sc.next().charAt(0);
+        switch(command){
+            case 'A':
+                System.out.println("best!!!");
+                break;
+            case 'B':
+                System.out.println("good!!");
+                break;
+            case 'C':
+                System.out.println("run!");
+                break;
+            case 'D':
+                System.out.println("slowly~");
+                break;
+            default:
+                System.out.println("what?");
+                break;
+
+        }
+    }
+}
---
+++ 1078_16646027(AC).java
@@ -0,0 +1,14 @@
+﻿import java.util.Scanner;
+public class Main{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int result=0;
+        for(int i=1;i<=a;i++){
+            if(i%2==0){
+                result= result+i;
+            }
+        }
+        System.out.println(result);
+    }
+}
