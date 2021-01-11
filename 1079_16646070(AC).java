66ac76a1a1ad1fbe3f69ea130d2e9c22376e3bbd
MoonSueLym <msr56501354@gmail.com>
2021-01-11 23:03:08 +0900

    codeup_

---
+++ 1079_16646070(AC).java
@@ -0,0 +1,16 @@
+﻿import java.util.Scanner;
+public class Main{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            char a = sc.next().charAt(0);
+            if(a!='q'){
+                System.out.println(a);
+            }
+            else{
+                System.out.println(a);
+                break;
+            }
+        }
+    }
+}
---
+++ 1080_16646234(AC).java
@@ -0,0 +1,21 @@
+﻿import java.util.Scanner;
+public class Main{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int add =0;
+     
+        for(int i=1;;i++){
+           add = add +i;
+            if(add>=a){
+                System.out.println(i);
+                break;
+            }  
+        }
+         
+            
+        
+            
+        
+    }
+}
