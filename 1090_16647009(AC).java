809ebc7de0fd9cae17c1d3448ec34ba8c771764b
MoonSueLym <msr56501354@gmail.com>
2021-01-11 23:38:41 +0900

    codeup_

---
+++ 1090_16647009(AC).java
@@ -0,0 +1,12 @@
+﻿import java.util.Scanner;
+public class Main{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int r = sc.nextInt();
+        int n = sc.nextInt();
+        double b = Math.pow(r,n-1);
+        System.out.println(a*(long)b);
+
+    }
+}
