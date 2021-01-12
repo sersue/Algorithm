82aa1731d8ac25fbfe14550ee60807635e8b3d02
MoonSueLym <msr56501354@gmail.com>
2021-01-12 09:22:24 +0900

    codeup_1094

---
+++ 1094_16648806(AC).java
@@ -0,0 +1,17 @@
+﻿import java.util.Scanner;
+import java.util.ArrayList;
+public class Main{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        ArrayList <Integer> attend = new ArrayList <Integer>();
+
+        for(int i=0;i<n;i++){
+            int k = sc.nextInt();
+            attend.add(k);
+        }
+        for(int i=n-1;i>=0;i--){
+            System.out.printf("%d ",attend.get(i));
+        }
+    }
+}
