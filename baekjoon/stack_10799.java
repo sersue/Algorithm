e7c7e7e166dd3d4a9fb15457f06c2d618546613b
MoonSueLym <msr56501354@gmail.com>
2021-02-22 09:03:47 +0900

    stack_10799

--- baekjoon/stack_10799.java
+++ baekjoon/stack_10799.java
@@ -2,14 +2,55 @@
 import java.io.*;
 import java.util.*;
 
-import jdk.internal.jline.internal.InputStreamReader;
-
 public class stack_10799 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String str = br.readLine();
+        Stack<Integer>stack = new Stack<Integer>();
+        int piece = 0;
 
+        for(int i=0;i<str.length();i++){
+            char c = str.charAt(i);
+            if(c == '('){
+                stack.push(i);
+            }
+            else{
+                if(stack.peek()+1==i){
+                    stack.pop();
+                    piece = piece+stack.size(); 
+
+                }
+                else{
+                    stack.pop();
+                    piece++;
+                }
+            }
+        }
+        System.out.println(piece);
+
+
+        //스택의 순서값이 필요 
+        // for(char ch : str.toCharArray()){
+        //     if(ch=='('){
+        //         stack.push(ch);
+
+        //     }
+
+        //     else{
+        //         stack.pop();
+        //         if(stack.isEmpty()){
+        //             piece = piece+0;
+        //         }
+        //         else{
+                    
+        //         }
+        //     }
+        // }
         
     }
+
     
 }
+
