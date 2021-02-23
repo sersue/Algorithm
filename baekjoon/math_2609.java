1c74963330ab4793e121a6e9f38f2d5965630d29
MoonSueLym <msr56501354@gmail.com>
2021-02-23 11:12:06 +0900

    math_10430

---
+++ baekjoon/math_2609.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class math_2609 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(gcd(a,b));
+    }
+
+    public int gcd(int a, int b){
+        if(b==0){
+            return 0;
+        }
+        return 
+    }
+    
+}
