a5a25ff20f211fb50900ba9c76d230e7d27eaaa7
MoonSueLym <msr56501354@gmail.com>
2021-02-23 19:16:23 +0900

    math_2609

--- baekjoon/math_2609.java
+++ baekjoon/math_2609.java
@@ -5,15 +5,25 @@ public class math_2609 {
 
         int a = sc.nextInt();
         int b = sc.nextInt();
-
-        System.out.println(gcd(a,b));
+        int gcd = gcd(a,b);
+        int lcm = lcm(a,b,gcd);
+        System.out.println(gcd);
+        System.out.println(lcm);
     }
 
-    public int gcd(int a, int b){
+    //최대 공약수 
+    public static int gcd(int a, int b){
         if(b==0){
-            return 0;
+            return a;
         }
-        return 
+        else{
+            return gcd(b,a%b);
+        }
+    }
+    //최소 공배수는 a*b/최대 공약수 로 나타낼 수 있다 
+    public static int lcm(int a,int b,int gcd){
+        int result = (a*b)/gcd;
+        return result;
     }
     
 }
